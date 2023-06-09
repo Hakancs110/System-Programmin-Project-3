37be50b2cdec1726b847c38272d3d8c59d441882
0xEnsar <ensaryozgat@gmail.com>
2023-06-09 23:14:45 +0300

    a

--- src/App.java
+++ src/App.java
@@ -558,7 +558,7 @@ public class App {
             writer.close();
             
         } catch (IOException e) {
-            System.err.println("Error writing RAM contents: " + e.getMessage());
+            System.err.println("Error writing cache contents: " + e.getMessage());
         }
     }
 
